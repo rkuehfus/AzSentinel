abfbc18149ce2b3314eaafad417e587b3f6b40c3
Robert Kuehfus <rkuehfus@live.com>
2019-06-17 20:30:46 -0400

    fix nsg

--- deploy.ps1
+++ deploy.ps1
@@ -11,7 +11,7 @@ $VaultName = $envPrefixName + 'MissionInstanceVault'
 New-AzKeyVault -VaultName $VaultName -ResourceGroupName $rg.ResourceGroupName -Location $rg.Location -EnabledForTemplateDeployment
 
 #Step 3: Add password as a secret.  Note:this will prompt you for a user and password.  User should be vmadmin and a password that meet the azure pwd police like P@ssw0rd123!!
-Set-AzureKeyVaultSecret -VaultName $VaultName -Name "VMPassword" -SecretValue (Get-Credential).Password
+Set-AzKeyVaultSecret -VaultName $VaultName -Name "VMPassword" -SecretValue (Get-Credential).Password
 
 #Step 4: Update Masterdeploy.parameters.json file with your envPrefixName and Key Vault info example- /subscriptions/{guid}/resourceGroups/{group-name}/providers/Microsoft.KeyVault/vaults/{vault-name}
 (Get-AzKeyVault -VaultName $VaultName).ResourceId
